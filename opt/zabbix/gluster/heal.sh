4e05d4c18fd33f6d89621d0ed54a61ba58916694
bbl <blka@users.noreply.github.com>
2017-06-01 22:35:33 +0200

    Update heal.sh
    seems to be more universal with $NF

--- opt/zabbix/gluster/heal.sh
+++ opt/zabbix/gluster/heal.sh
@@ -11,7 +11,7 @@ for volume in $volumes; do
   info="$(sudo gluster volume heal $volume info $check_heal 2>&1)"
   bricks="$(echo "$info" | grep Brick | awk '{print $2}')"
   for brick in $bricks; do
-    failed_count="$(echo "$info" | grep -A1 "Brick $brick" | grep 'Number of entries' | sed 's/.*\(.\)/\1/')"
+    failed_count="$(echo "$info" | grep -A1 "Brick $brick" | grep 'Number of entries' | awk '{print $NF}'"
     if [[ $failed_count -gt $threshold ]]; then
       message="$message Volume $volume, brick $brick, $check_heal: $failed_count
 "
