4f855b5276cdfcdceb87fd9ac64682f4641f0e21
F00b4rch <f00b4rch@gmail.com>
2018-06-11 15:54:33 +0200

    Correcting
    Removed previous copy/path

--- opt/zabbix/gluster/gluster_uuid.pl
+++ opt/zabbix/gluster/gluster_uuid.pl
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-set +m -o pipefail;
-
-set +m -o pipefail; if item=$(timeout -s 9 1 sudo gluster peer status 2>/dev/null); then echo "$item" | grep -c Disconnected; else echo '-1';
-
-fi
-root@puppet:/etc/puppetlabs/code/environments/production/modules/webtech/files/zabbix/userparams# cat gluster_uuid.pl#!/usr/bin/perl
+#!/usr/bin/perl
 # Ex.
 #       ./gluster_uuid.pl
 
